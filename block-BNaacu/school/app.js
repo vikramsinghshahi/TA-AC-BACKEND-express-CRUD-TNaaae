7542f15627c0ed2dde9dbd2c36be8d946f57dd52
Vikram Singh Shahi <vikramsinghshahi@gmail.com>
2021-09-13 13:25:30 +0530

    update

--- block-BNaacu/school/app.js
+++ block-BNaacu/school/app.js
@@ -2,7 +2,9 @@ var express = require("express");
 
 var mongoose = require("mongoose");
 
-var path = require("path")
+var studentsRouter = require("./routes/students");
+
+var path = require("path");
 
 mongoose.connect("mongodb://localhost/school", (err)=>{
     console.log(err? err: console.log("mogoose is connected"));
@@ -16,10 +18,21 @@ app.set("view engine" , "views")
 
 app.set("views", path.join(__dirname , "views"))
 
+app.use(express.urlencoded({extended:false}));
+
+
 app.get("/", (req, res)=>{
     res.render("index.ejs")
 })
 
+app.use("/students", studentsRouter);
+
+
+//hanle error
+
+app.use((req, res, next)=>{
+    res.status(404)/send("page not found")
+})
 
 
 app.listen(3000, ()=>{
