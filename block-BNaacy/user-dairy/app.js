b97ad3475ecadda3601560ed8c831703b8641811
Vikram Singh Shahi <vikramsinghshahi@gmail.com>
2021-09-14 12:30:22 +0530

    update

---
+++ block-BNaacy/user-dairy/app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var mongoose = require('mongoose');
+
+var path = require('path');
+
+var usersRouter = require('./routes/users');
+
+mongoose.connect('mongodb://localhost/user-dairy', (err) => {
+  console.log(err ? err : console.log('mongoose is connected'));
+});
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.use('/users', usersRouter);
+
+//error handling
+
+app.use((req, res, next) => {
+  res.status(404).send('page not found');
+});
+app.listen(3000, () => {
+  console.log('server is listening at 3k');
+});
